6e16a5d710468b319d94ddac886a2df612afba09
Bryam Jesus Talledo Garcia <btalledog@odybank.com.pe>
2022-10-14 21:55:20 -0500

    RegistroServices.js

--- src/services/RegistroServices.js
+++ src/services/RegistroServices.js
@@ -1,2 +1,29 @@
 import axios from "axios";
+const URL_API = `http://localhost:3000/registro`
+
+
+export const listarRegistro = async () => {
+  const result = await axios.get(URL_API);
+  return result
+}
+
+export const mostrarRegistro = async (id) => {
+  const result = await axios.get(`${URL_API}/${id}`);
+  return result
+}
+
+export const guardarRegistro = async (datos) => {
+  const result = await axios.post(URL_API, datos);
+  return result
+}
+
+export const editarRegistro = async (datos) => {
+  const result = await axios.put(`${URL_API}/${datos._id}`, datos);
+  return result
+}
+
+export const eliminarRegistro = async (id) => {
+  const result = await axios.delete(`${URL_API}/${id}`);
+  return result
+}
 
